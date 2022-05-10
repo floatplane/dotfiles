13a93661dd811d696419780c06296f9b68c0c0e5
Brian Sharon <bsharon@stripe.com>
2022-05-09 18:22:16 -0700

    Emacs native comp support

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,6 +16,29 @@
 ;; Make sure exec-path matches our environment's path
 (setq exec-path (split-string (getenv "PATH") path-separator))
 
+(if (and (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+    (progn
+	  (message "Native comp is available")
+      ;; Using Emacs.app/Contents/MacOS/bin since it was compiled with
+      ;; ./configure --prefix="$PWD/nextstep/Emacs.app/Contents/MacOS"
+      ;; Append to path to give priority to values from exec-path-from-shell-initialize.
+      (add-to-list 'exec-path (concat invocation-directory (file-name-as-directory "bin")) t)
+	  (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
+                                     (when (getenv "LIBRARY_PATH")
+                                       ":")
+				                     ;; This is where Homebrew puts libgccjit libraries.
+                                     (car (file-expand-wildcards
+                                           (expand-file-name
+                                            (concat
+                                             ;; brew --prefix returns either "/usr/local" or "/opt/homebrew"
+                                             (file-name-as-directory (string-trim (shell-command-to-string "brew --prefix")))
+                                             "opt/libgccjit/lib/gcc/*"))))))
+	  ;; Only set after LIBRARY_PATH can find gcc libraries.
+	  (setq comp-deferred-compilation t)
+      (setq comp-speed 3))
+  (message "Native comp is *not* available"))
+
 (let
     ((initpath (concat (file-name-directory load-file-name) (file-name-as-directory "init"))))
 
