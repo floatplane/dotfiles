17ae08b076f3dbc0a8cf489be7e8d4fd95b51f17
Brian Sharon <brian@floatplane.us>
2026-02-16 16:00:11 -0700

    worktree aware cleanup

--- bin/command.rb
+++ bin/command.rb
@@ -1,11 +1,12 @@
 module Command
-  def self.run(cmd, silent: false)
+  def self.run(cmd, silent: false, fatal: true)
     puts cmd unless silent
     result = `#{cmd}`
     unless $? == 0
       puts "Command failed with exit code #{$?}: `#{cmd}`"
       puts result
-      exit $?.exitstatus
+      exit $?.exitstatus if fatal
+      return nil
     end
     puts result unless silent
     result
